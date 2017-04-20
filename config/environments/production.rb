a577e34342171c52dfc5425fd2aa796c94b26d20
Kaan git <kaan_ozdmr@hotmail.com>
2017-04-20 19:17:14 +0000

    fixing mailers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,8 +9,8 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
- config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = {:host => 'https://mysterious-tundra-30682.herokuapp.com/', :protocol => 'https' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = {:host => 'mysterious-tundra-30682.herokuapp.com/', :protocol => 'https' }
   
   
   
